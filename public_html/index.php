69e357b9722e0ee46cab731e2dc4fede17bd7c38
Jeffrey Cooper <jcooper37@cnm.edu>
2017-04-27 10:05:30 -0600

    new site with index and css

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+	<link type="text/css" href="../css/styles.css" rel="stylesheet">
+	<title>Practice Site</title>
+</head>
+<body>
+	<header>Practice Site</header>
+	<h1>TITLE</h1>
+	<p>text</p>
+	<div>
+		<p>div</p>
+	</div>
+	<h2>h2</h2>
+	<h2 class="class-1">h2 class 1</h2>
+	<h2 id="id-1">h2 id 1</h2>
+</body>
